db34e0dcc7813c199c10df4e0e62e722042a5027
Yakymchuk Artur <Yakymchuk.Artur@formpipe.com>
2022-10-05 23:57:07 +0300

    removed comments

--- lab1/lab1_task2.rkt
+++ lab1/lab1_task2.rkt
@@ -7,27 +7,6 @@
     (else (cond ((even? (nums)) (display n)))
           n)
      ))
-    
-   
-
-
-
-    ;(if (even? n) ((display (nums)) n) (nums)))
-
-         
-   ; (cond
-    ;       ((even? (nums)) (display n) n)
-     ;      (else (nums))))
-
-
-
-
-
-         ;((display ((odd? (nums)) n " ")) n)))
-
-
-
-        ;(if (odd? n) ((display n) (nums)) (nums))
 
 (nums)
 
